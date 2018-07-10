625c9a8da5e24a00adab19f6783063c1f524243c
wachka06 <wachka06@gmail.com>
2018-07-10 14:53:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,9 +35,9 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
